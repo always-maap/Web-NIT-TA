873c4b08a1211a4ba846839b77e3be53962a9fb7
tur1ng <mohammadali.ap.2000@gmail.com>
2023-12-05 03:50:37 +0330

    article-api

---
+++ examples/articles-api/main.js
@@ -0,0 +1,211 @@
+import express from "express";
+import cors from "cors";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express API with Swagger",
+      version: "1.0.0",
+    },
+    components: {
+      schemas: {
+        Article: ArticleSchema,
+      },
+    },
+  },
+  apis: ["./main.js"],
+};
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let articles = [];
+
+/**
+ * @swagger
+ * /api/articles:
+ *   get:
+ *     summary: Returns the list of all the articles
+ *     responses:
+ *       200:
+ *         description: The list of the articles
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Article'
+ */
+app.get("/api/articles", (req, res) => {
+  return res.json(articles);
+});
+
+/**
+ * @swagger
+ * /api/articles:
+ *   post:
+ *     summary: Create a new article
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Article'
+ *     responses:
+ *       201:
+ *         description: The created article
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Article'
+ *       409:
+ *         description: An article with the same id already exists
+ */
+app.post("/api/articles", (req, res) => {
+  const article = new Article(req.body);
+  const exists = articles.some((a) => a.id === article.id);
+  if (exists) return res.status(409).send();
+  articles.push(article);
+  res.status(201).json(article);
+});
+
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   get:
+ *     summary: Get an article by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The article
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Article'
+ *       404:
+ *         description: Article not found
+ */
+app.get("/api/articles/:id", (req, res) => {
+  const article = articles.find((a) => a.id === req.params.id);
+  if (!article) return res.status(404).send();
+  res.json(article);
+});
+
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   put:
+ *     summary: Update an article by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Article'
+ *     responses:
+ *       200:
+ *         description: The updated article
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Article'
+ *       404:
+ *         description: Article not found
+ */
+app.put("/api/articles/:id", (req, res) => {
+  const article = articles.find((a) => a.id === req.params.id);
+  if (!article) return res.status(404).send();
+  Object.assign(article, req.body);
+  res.json(article);
+});
+
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   delete:
+ *     summary: Delete an article by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Article deleted successfully
+ *       404:
+ *         description: Article not found
+ */
+app.delete("/api/articles/:id", (req, res) => {
+  const index = articles.findIndex((a) => a.id === req.params.id);
+  if (index === -1) return res.status(404).send();
+  articles.splice(index, 1);
+  res.status(204).send();
+});
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
+
+// Path: Article helpers
+function guid() {
+  return Math.random().toString(36).substring(2, 15);
+}
+
+class Article {
+  constructor({
+    id = guid(),
+    title = "",
+    content = "",
+    author = "",
+    publishedAt = new Date(),
+  }) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.author = author;
+    this.publishedAt = publishedAt;
+  }
+}
+
+var ArticleSchema = {
+  type: "object",
+  properties: {
+    id: {
+      type: "string",
+      description: "Article id",
+    },
+    title: {
+      type: "string",
+      description: "Article title",
+    },
+    content: {
+      type: "string",
+      description: "Article content",
+    },
+    author: {
+      type: "string",
+      description: "Article author",
+    },
+    publishedAt: {
+      type: "string",
+      description: "Article published date",
+    },
+  },
+};
